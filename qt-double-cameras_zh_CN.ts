9485b9b32841d2823d30811a630d0b7e12b52b7d
Pai Peng <sipaipv6@gmail.com>
2022-04-12 23:49:45 +0800

    add text localization

--- qt-double-cameras_zh_CN.ts
+++ qt-double-cameras_zh_CN.ts
@@ -1,3 +1,25 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>CameraWindow</name>
+    <message>
+        <location filename="camera_window.ui" line="14"/>
+        <source>CameraWindow</source>
+        <translation type="unfinished">QT双摄像头</translation>
+    </message>
+    <message>
+        <location filename="camera_window.ui" line="36"/>
+        <location filename="camera_window.ui" line="99"/>
+        <location filename="camera_window.ui" line="169"/>
+        <source>TextLabel</source>
+        <translation type="unfinished">测试</translation>
+    </message>
+    <message>
+        <location filename="camera_window.ui" line="109"/>
+        <location filename="camera_window.ui" line="179"/>
+        <source>Start</source>
+        <translation type="unfinished">开始</translation>
+    </message>
+</context>
+</TS>
